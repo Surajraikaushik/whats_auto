95de8833ec26b1e73b6594982ef2c5cfdd998db5
Abhinay Rai <42450450+Surajraikaushik@users.noreply.github.com>
2025-05-25 11:27:05 +0530

    OK

--- script_autp.py
+++ script_autp.py
@@ -1,7 +1,7 @@
 #import pywhatkit
 
 #pywhatkit.sendwhatmsg("+91 9935709273",'Test',18 ,33)
-
+import time as t
 import pywhatkit
 
 phone_number = ['+91 9935709273','+91 9044836877']  # Replace with recipient's number (include country code)
@@ -10,4 +10,5 @@ for i in phone_number:
     message = "Good Morning!"
     pywhatkit.sendwhatmsg_instantly(i, message)
 
+t.sleep(60)
 print('Done ! Successfully!')
